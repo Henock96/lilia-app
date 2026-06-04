3580b85c8abb1a6ba668fc36878c775eb5c514c2
Henok Mipoka <mipokahenok@yahoo.fr>
2026-06-04 21:29:11 +0100

    feat: new commit client mobile

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ val mapsApiKey: String =
 android {
     namespace = "com.dreesis.lilia.lilia_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         // Flag to enable support for the new language APIs
--- lib/features/home/presentation/widgets/section/restaurant_card.dart
+++ lib/features/home/presentation/widgets/section/restaurant_card.dart
@@ -331,29 +331,3 @@ class RestaurantCard extends ConsumerWidget {
     );
   }
 }
-
-class _VisualBadge extends StatelessWidget {
-  final String label;
-  final Color color;
-
-  const _VisualBadge({required this.label, required this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Text(
-        label,
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 10,
-          fontWeight: FontWeight.w700,
-        ),
-      ),
-    );
-  }
-}
